a3e193f73767c63e8bde24184abd3fe6871a0399
Yusril Izzaddien <falih.izzaddien@gmail.com>
2023-10-24 09:48:55 +0700

    database seed

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+use Carbon\Carbon;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        // Buat beberapa user palsu
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'nama' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'sandi' => Hash::make('password'), // Sandi default: 'password'
+                'created_at' => \Carbon\Carbon::now(),
+                'updated_at' => \Carbon\Carbon::now(),
+            ]);
+        }
+    }
+}
